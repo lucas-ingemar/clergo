7a0aec32c2c0293730a228ccb8f150e284215a5e
Lucas Börjesson <lucas.ri.borjesson@gmail.com>
2023-06-30 16:01:20 +0200

    Fixed reading from home directory using ~

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,22 @@
 package config
 
+import (
+	"strings"
+
+	"github.com/adrg/xdg"
+)
+
 type Config struct {
 	LibPath string
 }
 
-var CONFIG = Config{
-	// FIXME
-	LibPath: "/home/lucas/.clergo",
+func initConfig(c Config) Config {
+	c.LibPath = strings.ReplaceAll(c.LibPath, "~", xdg.Home)
+	return c
 }
+
+var (
+	CONFIG = initConfig(Config{
+		LibPath: "~/.clergo",
+	})
+)
